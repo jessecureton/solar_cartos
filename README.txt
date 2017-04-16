d945ea3169bb6fc177b4bd21a925537352745bf7
Jesse Cureton <jlcvm6@mst.edu>
2017-04-16 13:57:56 -0500

    Documentation configuration and updated .gitignore

---
+++ README.txt
@@ -0,0 +1,10 @@
+/**
+ * @file README.txt
+ * @author Jesse Cureton (jlcvm6@mst.edu)
+ * @date 13 April 2017
+ * @copyright Copyright 2017 - Jesse Cureton. All rights reserved.
+ * @license
+ * 
+ * @mainpage The Solar Car Real Time Operating System (Solar CaRTOS)
+ * 
+ */
--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+/**
+ *  @file main.c
+ *  @author Jesse Cureton (jlcvm6@mst.edu)
+ *  @date 13 April 2017
+ *
+ *  @brief Primary OS entry point, initialization, and loop
+ *
+ *  @copyright Copyright 2017 - Jesse Cureton. All rights reserved.
+ *  @license
+ */
+
 #include "msp.h"
 
 int main(void)
