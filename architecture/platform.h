d30da7e7cadcd68a777e294e354798ee783d4121
Jesse Cureton <jlcvm6@mst.edu>
2017-04-30 22:24:08 -0500

    Update the platform API to require tracking SYSCLK current speed

--- architecture/platform.h
+++ architecture/platform.h
@@ -18,7 +18,7 @@
 #ifndef INCLUDE_PLATFORMS_H_
 #define INCLUDE_PLATFORMS_H_
 
-/**
+/*
  * Platform include definitions
  */
 #if defined (__MSP432P401R__)
@@ -32,9 +32,14 @@
 #error "Attempting to build for unsupported microcontroller!"
 #endif
 
-/**
+/*
  * API Implementation
  */
+
+/**
+ * Current system clock speed
+ */
+extern uint32_t PLATFORM_SYSCLK;
 
 /**
  * @brief Complete any processor-specific initialization
@@ -42,19 +47,20 @@
  * Outcomes:
  * - Watchdog timer disabled
  * - Global interrupts enabled
+ * - {@link platform_sysclk} updated to default speed for processor at RESET
  *
  * @return A {@link err_t system error code}
  */
 extern err_t platform_proc_init();
 
 /**
- * @brief Set main system clock in hertz.
+ * @brief Set main system clock (typically MCLK) as close as possible to the given value in hertz.
+ *
+ * Once the frequency has been set, {@link platform_sysclk} will be set to the new clock speed
+ *
  * @param hertz Desired processor speed in Hz
  * @return A {@link err_t system error code}
  */
 extern err_t platform_set_sysclk(uint32_t hertz);
-
-
-
 
 #endif /* INCLUDE_PLATFORMS_H_ */
